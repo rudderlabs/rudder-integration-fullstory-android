a9486d6b8072524e5e1cdeba8e23edde357f965c
1abhishekpandey <abhishekpandey10196@gmail.com>
2021-11-10 17:26:27 +0530

    Updated Factory class.

--- fullstory/src/main/java/com/rudderstack/android/integrations/fullstory/FullStoryIntegrationFactory.java
+++ fullstory/src/main/java/com/rudderstack/android/integrations/fullstory/FullStoryIntegrationFactory.java
@@ -12,8 +12,12 @@ import com.rudderstack.android.sdk.core.RudderIntegration;
 import com.rudderstack.android.sdk.core.RudderLogger;
 import com.rudderstack.android.sdk.core.RudderMessage;
 
+import java.math.BigInteger;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TimeZone;
 
 
 public class FullStoryIntegrationFactory extends RudderIntegration<RudderClient> {
@@ -50,23 +54,31 @@ public class FullStoryIntegrationFactory extends RudderIntegration<RudderClient>
         if (type != null) {
             switch (type) {
                 case MessageType.IDENTIFY:
-                    String userId = !TextUtils.isEmpty(element.getUserId()) ? element.getUserId() : element.getAnonymousId();
                     Map<String, Object> traits = new HashMap<>(element.getTraits());
                     if (!isEmpty(traits)){
                         traits.remove("userId");
                     }
-                    FS.identify(userId, traits);
+                    if (!TextUtils.isEmpty(element.getUserId())) {
+                        FS.identify(element.getUserId(), traits);
+                        return;
+                    }
+                    FS.setUserVars(traits);
                     break;
                 case MessageType.TRACK:
                     if (!TextUtils.isEmpty(element.getEventName())) {
-                        FS.event(getTrimKey(element.getEventName()), element.getProperties());
+                        FS.event(getTrimKey(element.getEventName()), getSuffixProperty(element.getProperties()));
                         return;
                     }
                     RudderLogger.logDebug("Event name is not present in the Track call. Hence, event not sent");
                     break;
                 case MessageType.SCREEN:
                     if (!TextUtils.isEmpty(element.getEventName())) {
-                        FS.event(getTrimKey("screen view " + element.getEventName()), element.getProperties());
+                        Map<String, Object> screenProperties = getSuffixProperty(element.getProperties());
+                        if (isEmpty(screenProperties)) {
+                            screenProperties = new HashMap<>();
+                        }
+                        screenProperties.put("name", element.getEventName());
+                        FS.event("Screen Viewed", screenProperties);
                         return;
                     }
                     RudderLogger.logDebug("Event name is not present in the Screen call. Hence, event not sent");
@@ -82,6 +94,38 @@ public class FullStoryIntegrationFactory extends RudderIntegration<RudderClient>
     public void reset() {
         FS.anonymize();
         RudderLogger.logVerbose("FS.anonymize();");
+    }
+
+    private Map<String, Object> getSuffixProperty(Map<String, Object> properties) {
+        if (isEmpty(properties)) {
+            return null;
+        }
+        Map<String, Object> suffixedProperty = new HashMap<>();
+        for (String key : properties.keySet()) {
+            Object item = properties.get(key);
+            key = key.trim().replace(" ", "_");
+            // default to no suffix;
+            String suffix = "";
+            if (item instanceof String || item instanceof Character) {
+                suffix = "_str";
+            } else if (item instanceof Number) {
+                // default to real
+                suffix = "_real";
+                if (item instanceof Integer || item instanceof BigInteger || item instanceof Long || item instanceof Short) {
+                    suffix = "_int";
+                }
+            } else if (item instanceof Boolean) {
+                suffix = "_bool";
+            } else if (item instanceof Date) {
+                suffix = "_date";
+                SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+                formatter.setTimeZone(TimeZone.getTimeZone("UTC"));
+                suffixedProperty.put(key + suffix, formatter.format(item));
+                continue;
+            }
+            suffixedProperty.put(key + suffix, item);
+        }
+        return suffixedProperty;
     }
 
     static String getTrimKey(String key) {
